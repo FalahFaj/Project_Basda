f055659a67fb7eba7a74d3a0ba231085dd13b30d
Muhammad Falah <144109406+FalahFaj@users.noreply.github.com>
2025-05-26 00:53:55 +0700

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-# else:
-#                     print("Jika anda tidak mengisi alamat maka anda tidak bisa melakukan penyewaan")
-#                     input("Tekan enter untuk melanjutkan")
-#                     clear_terminal()
