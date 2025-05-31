1309615ccb41c852644dd0694c8fc61f160309b2
Muhammad_Falah <muhammadfalahfajrul0@gmail.com>
2025-05-31 09:24:06 +0700

    ngubah host

--- koneksiDB.py
+++ koneksiDB.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def connect():
     try:
-        return psycopg2.connect("dbname='Cantiknya Mahar' user='postgres' host='125.161.165.56' password= 'ns4dpn'port='5432'")
+        return psycopg2.connect("dbname='Cantiknya Mahar' user='postgres' host='serverpostgre.duckdns.org' password= 'ns4dpn'port='5432'")
     except Exception as e:
         print("Koneksi Gagal")
         print(e)
